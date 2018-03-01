27bdd87a74817f36005114ca1d60d392865c17dc
xialing <despairvip@gmail>
2018-03-01 09:19:02 +0800

    数据表设计

--- kuaishou_admin/models.py
+++ kuaishou_admin/models.py
@@ -1,3 +1,96 @@
+import decimal
+
 from django.db import models
 
+
 # Create your models here.
+class Client(models.Model):
+    # 总消费
+    consume_gold = models.IntegerField()
+    # 联系方式
+    phone_num = models.IntegerField()
+    # 金币
+    gold = models.IntegerField()
+    # 用户名
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    '''
+    项目表
+    '''
+    pro_name = models.CharField(max_length=100, blank=False)
+    count_project = models.IntegerField()
+    pro_gold = models.DecimalField("积分", max_digits=19, decimal_places=10, default=decimal.Decimal('0.0'))
+    combo = models.ManyToManyField('Order_combo', blank=True)
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    '''
+    订单表
+    '''
+    choices_status = (
+        (0, "未开始"),
+        (1, "执行中"),
+        (2, "已完成"),
+        (3, "异常单"),
+        (4, "已撤单"),
+    )
+    client = models.ForeignKey('Client')
+    project = models.ForeignKey('Project')
+
+    gold = models.DecimalField("积分", max_digits=19, decimal_places=10, default=decimal.Decimal('0.0'))
+    data = models.TextField(default="")
+    create_date = models.DateTimeField(auto_now_add=True)
+    showdata = models.TextField(default="")
+    count_init = models.IntegerField(default=0)
+    type_id = models.IntegerField(default=0)
+    kuaishou_id = models.CharField(max_length=20)
+    link_works = models.URLField()
+    status = models.IntegerField(default=0, choices=choices_status)
+    order_id_num = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    def to_dict(self):
+
+        order_dict ={
+            "order_id": self.order_id_num,
+            "project_name": self.project.pro_name,
+            "kuaishou_id":self.kuaishou_id,
+            "ordered_num": self.count_init,
+            "status_order": self.status,
+            "production_link": self.link_works,
+            "user_name": self.client.name,
+            "create_order_time": self.create_date
+        }
+        return order_dict
+
+class Order_combo(models.Model):
+    '''
+    套餐表
+    '''
+    name = models.CharField(max_length=100, null=False)
+    detail_combo = models.ManyToManyField('Project', blank=True)
+    pro_gold = models.DecimalField("积分", max_digits=19, decimal_places=10, default=decimal.Decimal('0.0'))
+
+    def __str__(self):
+        return self.name
+
+
+class Old_Order_project(models.Model):
+    '''
+    扩展表  历史记录表
+    '''
+    old_gold = models.DecimalField("积分", max_digits=19, decimal_places=10, default=decimal.Decimal('0.0'))
+
+    orders = models.OneToOneField('Order', null=True)
+
+    def __str__(self):
+        return self.name
