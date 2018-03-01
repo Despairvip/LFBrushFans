0af921b2b76a6f8fcb47caae188a4af3113c8e5b
xialing <despairvip@gmail>
2018-03-01 09:34:50 +0800

    配置数据库

--- Demo/settings.py
+++ Demo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'kuaishou_admin'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -76,10 +77,27 @@ WSGI_APPLICATION = 'Demo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LFBrushFans',
+        'USER':'root',
+        'PASSWORD':'mysql',
+        'HOST':'127.0.0.1',
+        'PORT':'3306'
     }
 }
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://10.211.55.5:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+# Session
+
+# SESSION_ENGINE = "django.contrib.sessions.backends.db"
 
 
 # Internationalization
