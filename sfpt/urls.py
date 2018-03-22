e573072aebf68c1dfe95b498c20289b25622a45d
xialing <despairvip@gmail>
2018-03-22 18:00:31 +0800

    测试

--- sfpt/urls.py
+++ sfpt/urls.py
@@ -38,7 +38,5 @@ urlpatterns = [
     url(r'^deleteTaocan',views.deleteTaocan),
     url(r'^login_admin',views.login_houtai),
     url(r'^index',views.index),
-    # url(r'^delProInCom',views.delete_taocan_project),
     url(r'^showAll',views.ShowAll),
-    url(r'^test',views.changeTaocanTest),
 ]
